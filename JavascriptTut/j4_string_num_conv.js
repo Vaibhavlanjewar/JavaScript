8db7f1be48bded93d65f41287236deba1a3d325d
VaibhavLanjewar <vlanjewar523@gmail.com>
2024-02-12 17:51:46 +0530

    12-02-24

---
+++ JavascriptTut/j4_string_num_conv.js
@@ -0,0 +1,34 @@
+let someNumber=33;
+
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
+
+// ***** operation ******
+
+let val=3;
+let negval=- val;
+console.log(negval);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2); //pow 
+console.log(10%5); //mod .. remainder  of the division
+
+let str1 = "hello"
+let str2= " vnbl"
+let str3=str1 + str2 ;
+console.log(str3);
+console.log("1" + 3); // 
+console.log(1+"3");
+console.log("1" + "3");
+console.log("1" + "3" + "6"); 
+console.log(1+2+"3");
+
+console.log(true);
+console.log(+true);
+
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
---
+++ JavascriptTut/j5_comaparisions.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(6<1)
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(1>="2");
+
+console.log(null>0);  // false 
+console.log(null ==0);
+console.log(null >=0);  // here val convesrion problems comes 
+
+console.log(undefined>0);   //false
+console.log(undefined==0);  // false 
+console.log(undefined<0);  // false 
+
+// === strict check , it checked the datatype 
+console.log("2" === 2)
+console.log(2===2);
